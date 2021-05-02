ed283e68e35fb43d4a283107855ddf0bfc4b6c86
shipperizer <alexcabb@gmail.com>
2021-05-02 08:53:50 +0100

    fix: adjust monitor http api to API interface

--- pkg/monitor/prometheus.go
+++ pkg/monitor/prometheus.go
@@ -2,13 +2,11 @@ package monitor
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-// MetricsServer creates a router with an endpoint for prometheus on api/v1/metrics
 func MetricsServer() *mux.Router {
 	router := mux.NewRouter()
-	router.PathPrefix("/api/v1/metrics").Handler(promhttp.Handler())
+	NewMonitorAPI().Routes(router)
 
 	return router
 }
