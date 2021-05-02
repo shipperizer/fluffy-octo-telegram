625a2433fcaaafc9928042c2d9a192c9ec9066d9
shipperizer <alexcabb@gmail.com>
2021-05-02 08:54:43 +0100

    feat: add jwk endpoint, closes #5

---
+++ pkg/jwks/api.go
@@ -0,0 +1,32 @@
+package jwks
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/lestrrat-go/jwx/jwk"
+
+	"github.com/shipperizer/fluffy-octo-telegram/pkg/api"
+)
+
+type jwkAPI struct {
+	jwkSet jwk.Set
+}
+
+func (a *jwkAPI) Routes(router *mux.Router) {
+	router.HandleFunc("/api/.well-known/jwks.json", a.ListJWKs).Methods(http.MethodGet, http.MethodOptions)
+}
+
+func (a *jwkAPI) ListJWKs(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(a.jwkSet)
+}
+
+func NewJWKAPI(jwkPub jwk.Set) api.API {
+	return &jwkAPI{
+		jwkSet: jwkPub,
+	}
+}
---
+++ pkg/jwks/utils.go
@@ -0,0 +1,66 @@
+package jwks
+
+import (
+	"fmt"
+	"io/ioutil"
+	"sync"
+
+	"github.com/lestrrat-go/jwx/jwk"
+	log "github.com/sirupsen/logrus"
+)
+
+func LoadKeyFromFile(filename string) ([]byte, error) {
+	key, err := ioutil.ReadFile(filename)
+	return key, err
+}
+
+func LoadJWKs(pubPath, privPath string) (jwk.Set, jwk.Key, error) {
+	var set jwk.Set
+	var key jwk.Key
+
+	var wg sync.WaitGroup
+	errs := make([]error, 0)
+
+	wg.Add(2)
+
+	go func() {
+		var err error
+
+		set, err = jwk.ReadFile(pubPath)
+
+		if err != nil {
+			log.Errorf("cannot find the file %s: %v", pubPath, err)
+			errs = append(errs, err)
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		var err error
+
+		keyRaw, err := LoadKeyFromFile(privPath)
+
+		if err != nil {
+			log.Errorf("cannot find the file %s: %v", privPath, err)
+			errs = append(errs, err)
+		}
+
+		key, err = jwk.ParseKey(keyRaw)
+
+		if err != nil {
+			errs = append(errs, err)
+		}
+
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	for _, e := range errs {
+		if e != nil {
+			return set, key, fmt.Errorf("error fetching keys: %v", errs)
+		}
+	}
+
+	return set, key, nil
+}
